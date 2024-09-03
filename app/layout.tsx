648503747581babf3da0c0d068da26a7f0dc5974
Camila Arce <arce.b.camila@gmail.com>
2024-09-03 20:14:22 -0300

    crear estructura básica de proyecto

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Camila Arce",
+  description: "Portfolio personal de Camila Arce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { IoHomeOutline } from "react-icons/io5";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full"> 
+        <FloatingNav navItems={[
+            { name: "Inicio", link: "/", icon: <IoHomeOutline /> },
+          ]}
+          className="hidden md:flex"
+        />
+        <Hero />  
+        <Grid />
+      </div>
+    </main>
+  );
+}
